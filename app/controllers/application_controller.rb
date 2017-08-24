4e04685e36353a1d2c65b0e6734c62ed15f31424
Jasmine Tan <jasminetanom@gmail.com>
2017-08-24 13:02:56 +0800

    Added recipes controller and refactored code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,7 @@ class ApplicationController < ActionController::Base
 # if user is logged in, return current_user, else return guest_user
   def current_or_guest_user
     if current_user
-      if session[:guest_user_id] && session[:guest_user_id] != current_user.id
-        logging_in
-        # reload guest_user to prevent caching problems before destruction
-        guest_user(with_retry = false).try(:reload).try(:destroy)
-        session[:guest_user_id] = nil
-      end
+      transfer_session_to_logged_in_user if session[:guest_user_id].present?
       current_user
     else
       guest_user
@@ -29,7 +24,7 @@ class ApplicationController < ActionController::Base
   # creating one as needed
   def guest_user(with_retry = true)
     # Cache the value the first time it's gotten.
-    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+    @cached_guest_user ||= User.find(guest_user_id)
 
   rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
      session[:guest_user_id] = nil
@@ -38,26 +33,35 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # called (once) when the user logs in, insert any code your application needs
-  # to hand off from guest_user to current_user.
-  def logging_in
-    # For example:
-    # guest_comments = guest_user.comments.all
-    # guest_comments.each do |comment|
-      # comment.user_id = current_user.id
-      # comment.save!
-    # end
-
-      guest_dog = guest_user.dog
-      guest_dog.user_id = current_user.id
-      guest_dog.save!
+  def guest_user_id
+    session[:guest_user_id] ||= create_guest_user.id
   end
 
   def create_guest_user
     u = User.create(:first_name => "guest", :last_name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
     u.save!(:validate => false)
-    session[:guest_user_id] = u.id
     u
+  end
+
+  def transfer_session_to_logged_in_user
+    if session[:guest_user_id] != current_user.id
+      transfer_dog
+      destroy_guest_user
+    end
+  end
+
+  # called (once) when the user logs in, insert any code your application needs
+  # to hand off from guest_user to current_user.
+  def transfer_dog
+      guest_dog = guest_user.dog
+      guest_dog.user_id = current_user.id
+      guest_dog.save!
+  end
+
+  # reload guest_user to prevent caching problems before destruction
+  def destroy_guest_user
+    guest_user(with_retry = false).try(:reload).try(:destroy)
+    session[:guest_user_id] = nil
   end
 
 end
