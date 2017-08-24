1934f263d3d847b6fa68f0ee8ba88b6711c009b2
Jamie Tan <jamie.enxuan@gmail.com>
2017-08-24 14:20:43 +0800

    tidied up banner bottom

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-\# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -114,10 +114,6 @@ ActiveRecord::Schema.define(version: 20170823091558) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
