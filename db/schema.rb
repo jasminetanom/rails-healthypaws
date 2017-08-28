ac07ca4ae0ea03521403ad9d889adaf31e7e2978
Jamie Tan <jamie.enxuan@gmail.com>
2017-08-28 17:45:07 +0800

    user profile done, still needs design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825063315) do
+ActiveRecord::Schema.define(version: 20170828085255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20170825063315) do
     t.bigint "dog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["dog_id"], name: "index_recipes_on_dog_id"
   end
 
